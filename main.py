35c7996180e40b020de17d89feb52830f71de5bc
nicofilliol <nicolas.filliol@outlook.com>
2020-04-10 12:45:02 +0200

    modified to open electron from folder

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from __future__ import print_function  # For Py2/3 compatibility
 import eel
 
 # Set web files folder
@@ -7,16 +6,10 @@ eel.init('web')
 
 @eel.expose                         # Expose this function to Javascript
 def say_hello_py(x):
-    print('Hello from %s' % x)
+    print(f'Hello from {x}')
 
 
 say_hello_py('Python World!')
 eel.say_hello_js('Python World!')   # Call a Javascript function
 
-
-options = {
-    'mode': 'electron'
-}
-
-eel.start('index.html', options=options)
-#eel.start('hello.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron.exe', '.'])
+eel.start('index.html', mode='electron', cmdline_args=['electron'])
